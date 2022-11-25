78b6f9960f197bd92e30f91a7011dd503a335436
DPR <mohammad.01101101@gmail.com>
2022-11-25 17:43:37 +0330

    [UPDATE]: config nvim-tree

---
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,24 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+    hijack_cursor = true,
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+    renderer = {
+        highlight_git = true,
+    },
+    git = {
+        enable = true,
+        ignore = true,
+    }
+})
